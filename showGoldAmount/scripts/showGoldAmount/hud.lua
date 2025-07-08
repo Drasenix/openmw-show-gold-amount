d7c2de12b37b485f9218e90eec0c852fd3d47432
Drasenix <adowns@hotmail.fr>
2025-07-08 03:12:21 +0200

    hide in menu and positionning

--- showGoldAmount/scripts/showGoldAmount/hud.lua
+++ showGoldAmount/scripts/showGoldAmount/hud.lua
@@ -1,5 +1,6 @@
 local ui = require('openmw.ui')
 local self = require('openmw.self')
+local core = require('openmw.core')
 local types = require('openmw.types')
 local util = require("openmw.util")
 local I = require('openmw.interfaces')
@@ -9,29 +10,31 @@ local I = require('openmw.interfaces')
 
 local selfObject = self
 local element = nil
-local function onUpdate(dt)
+local function onFrame(dt)
    if element ~= nil then
       element:destroy()
    end
-   local playerInventory = types.Actor.inventory(self.object)
-   local goldAmount = playerInventory:countOf('gold_001')
-   element = ui.create({
-      template = I.MWUI.templates.textNormal,
-      layer = "Windows",
-      type = ui.TYPE.Text,
-      props = {
-         text = tostring(goldAmount) .. " gold",         
-         textSize = 20,
-         relativePosition = util.vector2(0.16, 0.95),
-         
-         visible = true,
-      },
-   })
+   local windowOpened = core.isWorldPaused()
+   if not windowOpened then 
+      local playerInventory = types.Actor.inventory(self.object)
+      local goldAmount = playerInventory:countOf('gold_001')
+      element = ui.create({
+         template = I.MWUI.templates.textNormal,
+         layer = "Windows",
+         type = ui.TYPE.Text,
+         props = {
+            text = tostring(goldAmount) .. " gold",         
+            textSize = 20,
+            relativePosition = util.vector2(0.925, 0.975),         
+            visible = true,
+         },
+      })
+   end
 end
 
 
 return {
    engineHandlers = {
-      onUpdate = onUpdate
+      onFrame = onFrame
    }
 }
